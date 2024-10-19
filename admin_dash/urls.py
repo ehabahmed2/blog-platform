545f2a44e49d57e538325d7af7a4f8231a4e6e0f
Ehab <ihapbpc@gmail.com>
2024-10-19 23:56:15 +0200

    added the user managmenet page

--- admin_dash/urls.py
+++ admin_dash/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.dash, name='dash'),
     path('register_author/', views.reg_author, name='reg_author'),
+    path('manage_authors/', views.manage_authors, name='manage_authors'),
     path('logout/', views.logout_user, name='logout'),
 ]
--- admin_dash/views.py
+++ admin_dash/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CustomCreationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.views import LogoutView
+from .models import CustomUser
 
 
 # Create your views here.
@@ -39,4 +40,19 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'Logged out!')
     return redirect('home')
+
+
+@login_required
+def manage_authors(request):
+    user = request.user
+    if user.is_superuser:
+        authors = CustomUser.objects.filter(is_staff=True)
+        # delete author
+        if request.POST:
+            num = request.POST['num']
+            deleted_author = CustomUser.objects.filter(id=num)
+            deleted_author.delete()
+            messages.success(request, 'Deleted Succeffully!')
+            return redirect('manage_authors')
+    return render(request, 'users/manage_authors.html', {'authors': authors})
 
